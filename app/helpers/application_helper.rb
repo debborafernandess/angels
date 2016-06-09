9e17369255cd997f0038ba608bc40a4f96a46274
Debora e Bruno <debborafernandess@gmail.com>
2016-06-09 19:47:30 -0300

    Add default group to market via seed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def investors_name(group)
+    return 'Nenhum investidor faz parte deste grupo' if group.investors.count.zero?
     group.investors.pluck(:name).map(&:capitalize).join(', ')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@ Market.create(name: 'Design', description: 'Estudo da Federação das Indústria
 Market.create(name: 'Publicidade', description: 'O Brasil está entre os dez maiores mercados da publicidade e propaganda do mundo. Estamos falando de uma área que já movimentou, só neste ano, mesmo com a economia em recessão, mais de R$ 60 bilhões.')
 Market.create(name: 'Saúde', description: 'Para o crescimento e a conquista de um bom modelo de negócio em saúde,  envolvem capturar novas fontes de crescimentos (novos produtos, novos mercados, novos ambientes), correr pela eficiência operacional, estar inserido na onda digital e vencer a guerra por talentos.')
 Market.create(name: 'Tecnologia', description: 'O mercado de tecnologia no Brasil terá crescimento de 2,6% em dólares da receita em 2016. O percentual é metade do avanço registrado em 2015 e representa um dos piores desempenhos na história do setor, segundo a IDC.')
+
+Group.create(name: 'Investidores em Beleza', description: 'Interessadores em Beleza')
+Group.create(name: 'Investidores em Design', description: 'Interessadores em Design')
+Group.create(name: 'Investidores em Publicidade', description: 'Interessadores em Publicidade')
+Group.create(name: 'Investidores em Saúde', description: 'Interessadores em Saúde')
+Group.create(name: 'Investidores em Tecnologia', description: 'Interessadores em Tecnologia')
